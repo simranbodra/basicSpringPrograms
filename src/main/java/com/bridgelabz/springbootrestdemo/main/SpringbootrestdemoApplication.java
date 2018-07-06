4340149f4163ebd71d536bded5e44fb0a381142b
simranbodra <simranbodra6@gmail.com>
2018-07-06 19:51:11 +0530

    basic program for springboot learning

---
+++ src/main/java/com/bridgelabz/springbootrestdemo/main/SpringbootrestdemoApplication.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.springbootrestdemo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrestdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrestdemoApplication.class, args);
+	}
+}
